70b15316e8bea0ecf7c0f6e3ecb82ef45b3ecc08
mcakyerima <mcakyerima@gmail.com>
2023-03-18 00:32:08 +0100

    update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -16,12 +16,20 @@ int main(void)
 {
 
 char letter = 'a';
+char upper_case = 'A';
 
 while (letter <= 'z')
 {
 putchar(letter);
 letter++;
 }
+
+while (upper_case <= 'Z')
+{
+putchar(upper_case);
+upper_case++;
+}
+
 putchar('\n');
 return (0);
 }
