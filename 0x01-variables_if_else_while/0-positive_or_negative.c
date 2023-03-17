69abaeebe3d4ac08c81e5018bb895fa6ef7ab357
mcakyerima <mcakyerima@gmail.com>
2023-03-17 21:26:33 +0100

    update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,8 +5,8 @@
 int main(void)
 {
 int n;
-srand(tie(0));
-n =rand() - RAMD_MAX / 2;
+srand(time(0));
+n = rand() - RAMD_MAX / 2;
 const char* str = (n > 1) ? "positive" : "negative";
 printf("n is %s\n", str);
 return (0);
