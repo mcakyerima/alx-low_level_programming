67f73cbef822a0ec8e4df63e3b72d56ff97c9340
mcakyerima <mcakyerima@gmail.com>
2023-04-04 02:20:02 +0100

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _memset - fills n bytes f memory with a given constant value
+* @s: pointer to the memory area
+* @n: number of bytes to fill
+* @b: number of bytes to fill memory with
+* Retrun: pointer to the memory area
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+    *(s + i) = b;
+}
+    return (s);
+
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* _putchar - puts a character to the standart output
+* @c : the character to put to the standart output
+*/
+
+int _putchar(char c);
+
+/** _memset - fills n bytes of memory with a constant value
+* @s: the pointer to memory area
+* @b: constant value
+* @n: number of bytes to fill
+* Return: memory location
+*/
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
