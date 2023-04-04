727d09366d869a97427b1c1fd57b906b6e8e8c24
mcakyerima <mcakyerima@gmail.com>
2023-04-04 02:36:02 +0100

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,7 +5,7 @@
 * @s: pointer to the memory area
 * @n: number of bytes to fill
 * @b: number of bytes to fill memory with
-* Retrun: pointer to the memory area
+* Return: pointer to the memory area
 */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -13,8 +13,8 @@ char *_memset(char *s, char b, unsigned int n)
 int i;
 for (i = 0; i < n; i++)
 {
-    *(s + i) = b;
+*(s + i) = b;
 }
-    return (s);
+return (s);
 
 }
