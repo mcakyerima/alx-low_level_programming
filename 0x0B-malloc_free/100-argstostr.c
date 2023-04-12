7ab702494ccec4cb7c1f349d5d6ca6284bd275f0
mcakyerima <mcakyerima@gmail.com>
2023-04-12 02:15:42 +0100

    argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,42 +1,63 @@
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- * argstostr - Concatenates all the arguments of the program
- * @ac: Number of command line arguments
- * @av: Array of pointers to the command line arguments
- *
- * Return: Pointer to a new string containing all the arguments,
- *         separated by a newline character, or NULL if it fails
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+		len++, s++;
+
+	return (len);
+}
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * @ac: argc
+ * @av: arguments
+ * Return: pointer to array
+ */
+
 char *argstostr(int ac, char **av)
 {
-	char *str;
-	int i, j, len = 0, total_len = 0;
+	char *s;
+	int len = 0, i, j, k = 0;
 
-	if (ac == 0 || av == NULL)
+	if (ac == 0 || av == NULL) /* validate input */
 		return (NULL);
 
-	/* Calculate the total length of the concatenated string */
+	/* find length to malloc */
 	for (i = 0; i < ac; i++)
-		total_len += strlen(av[i]) + 1; /* Add 1 for the newline character */
-
-	/* Allocate memory for the concatenated string */
-	str = malloc(sizeof(char) * total_len);
-	if (str == NULL)
-		return (NULL);
-
-	/* Copy each argument to the concatenated string */
-	/* separated by a newline character */
-	for (i = 0, j = 0; i < ac; i++)
 	{
-		len = strlen(av[i]);
-		memcpy(str + j, av[i], len);
-		j += len;
-		str[j++] = '\n'; /* Add a newline character after each argument */
+		len += _strlen(av[i]);
+	}
+	len += (ac + 1); /* add space for newlines and null terminator */
+
+	/* allocate memory and free if error */
+	s = malloc(len * sizeof(char));
+
+	if (s == NULL)
+	{
+		free(s);
+		return (NULL);
 	}
 
-	str[j-1] = '\0'; /* Add a null terminator to the end of the string */
+	/* insert each arg into *str */
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j < _strlen(av[i]); j++)
+		{
+			s[k++] = av[i][j];
+		}
+		s[k++] = '\n';
+	}
 
-	return (str);
+	return (s);
 }
