3a2d6efc831e0762583b816d5a303be0d15256eb
mcakyerima <mcakyerima@gmail.com>
2023-04-18 00:42:20 +0100

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef 0-OBJECT_LIKE_MACRO_H
+#define 0-OBJECT_LIKE_MACRO_H
+
+/*
+ * File: 1-pi.h
+ * Author: Mohammed Abba
+ * Description: This header file defines the macro PI as an
+ *		 abbreviation for the value 3.14159265359.
+ */
+
+#define SIZE 1021;
+
+#endif /* SIZE */
