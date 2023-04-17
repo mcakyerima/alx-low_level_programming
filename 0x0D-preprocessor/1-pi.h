c7e1621de92dee92ba8419547b32f69ead9d1366
mcakyerima <mcakyerima@gmail.com>
2023-04-18 00:55:42 +0100

    1-pi.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * File: 1-pi.h
+ * Author: Mohammed Abba
+ * Description: This header file defines the macro PI as an
+ *               abbreviation for the value 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif /* PI_H */
