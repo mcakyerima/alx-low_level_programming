1c02ef4d077e1f7aa5e2d01ad30f270b01933b7f
mcakyerima <mcakyerima@gmail.com>
2023-04-18 05:59:26 +0100

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,3 +1,5 @@
+refactor this code make it more descriptive but dont change the function name
+
 /*
  * File: 4-new_dog.c
  * Author: Mohammed Abba
@@ -5,59 +7,16 @@
 
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "dog.h"
+
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
 
 /**
- * new_dog - creates a new dog
+ * _strlen - Finds the length of a string.
+ * @str: The string to be measured.
  *
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Return: pointer to the new dog, NULL if function fails
- */
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *new_dog;
-	int name_len, owner_len;
-
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-		return (NULL);
-
-	name_len = _strlen(name);
-	owner_len = _strlen(owner);
-
-	new_dog->name = malloc(name_len + 1);
-	if (new_dog->name == NULL)
-	{
-		free(new_dog);
-		return (NULL);
-	}
-
-	new_dog->owner = malloc(owner_len + 1);
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
-
-	_strcpy(new_dog->name, name);
-	_strcpy(new_dog->owner, owner);
-	new_dog->age = age;
-
-	return (new_dog);
-}
-
-/**
- * _strlen - gets the length of a string
- *
- * @str: the string to get the length of
- *
- * Return: length of the string
+ * Return: The length of the string.
  */
 int _strlen(char *str)
 {
@@ -70,21 +29,62 @@ int _strlen(char *str)
 }
 
 /**
- * _strcpy - copies a string from source to destination
+ * _strcopy - Copies a string pointed to by src, including the
+ *            terminating null byte, to a buffer pointed to by dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
  *
- * @dest: destination buffer for string copy
- * @src: source string to copy
- *
- * Return: pointer to destination buffer
+ * Return: The pointer to dest.
  */
-char *_strcpy(char *dest, char *src)
+char *_strcopy(char *dest, char *src)
 {
-	char *start = dest;
+	int index = 0;
 
-	while (*src)
-		*dest++ = *src++;
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
 
-	*dest = '\0';
+	dest[index] = '\0';
 
-	return (start);
+	return (dest);
+}
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: The new struct dog.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggy;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
+	doggy = malloc(sizeof(dog_t));
+	if (doggy == NULL)
+		return (NULL);
+
+	doggy->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (doggy->name == NULL)
+	{
+		free(doggy);
+		return (NULL);
+	}
+
+	doggy->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (doggy->owner == NULL)
+	{
+		free(doggy->name);
+		free(doggy);
+		return (NULL);
+	}
+
+	doggy->name = _strcopy(doggy->name, name);
+	doggy->age = age;
+	doggy->owner = _strcopy(doggy->owner, owner);
+
+	return (doggy);
 }
