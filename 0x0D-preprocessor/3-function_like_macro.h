eb8dec44f41d66167051b94f1a5dc6181c40f74e
mcakyerima <mcakyerima@gmail.com>
2023-04-18 01:09:30 +0100

    3-function_like_macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+/*
+ * File: 3-function_like_macro.h
+ * Author: Mohammed Abba
+ * Description: Header file that defines a function-like macro ABS(x)
+ *              that computes the absolute value of a number x.
+ */
+
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
