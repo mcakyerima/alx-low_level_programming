824686cfe2243f0231e993d279408a7decd693bf
mcakyerima <mcakyerima@gmail.com>
2023-04-18 01:21:49 +0100

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,22 @@
+#ifndef _4_SUM_H_
+#define _4_SUM_H_
+/*
+ * File: 4-sum.h
+ * Author: Mohammed Abba
+ * Description: This header file defines a function-like macro SUM(x, y)
+ *              that computes the sum of the two numbers x and y.
+ */
+
+
+/**
+ * SUM - Computes the sum of two numbers.
+ * @x: The first number.
+ * @y: The second number.
+ *
+ * Return: The sum of the two numbers.
+ */
+
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /* _4_SUM_H_ */
