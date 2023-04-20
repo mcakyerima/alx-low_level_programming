6297196573911ae6356b60fc6eab3ba006a2f559
mcakyerima <mcakyerima@gmail.com>
2023-04-20 01:32:10 +0100

    fix

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -6,6 +6,12 @@
 
 #include "3-calc.h"
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 /**
  * op_add - Returns the sum or two numbers
  * @a: Fist Number.
