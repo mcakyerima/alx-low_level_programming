dc06162e315b436bf9cba7870dca38e177accfda
mcakyerima <mcakyerima@gmail.com>
2023-04-20 05:38:26 +0100

    fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -30,10 +30,11 @@ int (*get_op_func(char *s))(int, int)
 
 	int i;
 
-	for (i = 0; ops[i].op != NULL; i++)
+	while (ops[i].op != NULL)
 	{
 		if (*(ops[i].op) == *s)
 			return (ops[i].f);
+		i++;
 	}
 
 	return (NULL);
