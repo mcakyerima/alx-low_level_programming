a5d8d89106eb7acbe9726259cace78de2a415262
mcakyerima <mcakyerima@gmail.com>
2023-05-02 22:22:44 +0100

    degub

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -17,7 +17,7 @@
  *	   else - the address of the new node.
  */
 
-listint_t *add_nodeint(listint_t **head, cont int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
 
