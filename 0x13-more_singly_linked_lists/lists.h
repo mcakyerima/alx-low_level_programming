a61708ca676cd7520db78db05ef8b520e3aed9b7
mcakyerima <mcakyerima@gmail.com>
2023-05-02 18:11:26 +0100

    updated prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,12 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/*
+ * File_name: lists.h
+ * Author: Mohammed Ak Yerima
+ * Description: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x13-more_singly_linked_lists directory.
+ */
 
 /**
  * struct listint_s - singly linked list
@@ -17,5 +23,9 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
+
+/* all function prototypes */
+size_t print_listint(const listint_t *h);
+
 
 #endif LISTS_H /*END OF LISTS_h*/
