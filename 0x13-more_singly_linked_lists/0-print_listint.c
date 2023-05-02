429e9e30196ae640bd6e692b4d92532332dbe502
mcakyerima <mcakyerima@gmail.com>
2023-05-02 17:52:04 +0100

    0-print_listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,32 @@
+/*
+ * File_name: 0-print_listint.c
+ * Author: Mohammed Ak Yerima
+ * Description: A program that prints all the elements of a listint_t list.
+ */
+
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the emlement of a listint_t list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: Number of Nodes in the listint_t.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	listint_t *current = h;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+
+		current = current->next;
+		nodes++;
+	}
+
+	return (nodes);
+}
