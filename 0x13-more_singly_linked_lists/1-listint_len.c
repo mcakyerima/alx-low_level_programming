2a6678b104ad40983ca43700dae8ab0abd395192
mcakyerima <mcakyerima@gmail.com>
2023-05-03 02:55:26 +0100

    1-listint-fix

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,14 +1,15 @@
-#include "lists.h"
-#include <stddef.h>
-
 /*
  * File_name: 1-listint_len.c
  * Author: Mohammed Ak Yerima.
  * Description: Function that returns the number of elements in a
  *		listint_t list.
+ */
+
+#include "lists.h"
+#include <stdio.h>
 
 /**
- * listint_len - Returns the number of elements in a linked listint_t list.
+ * listint_len - Returns number of elements in a linked listint_t list.
  * @h: A pointer to the head of the linked list.
  *
  * Return: The number of elements in the linked list.
