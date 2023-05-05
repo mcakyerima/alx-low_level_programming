6eb40d7329d86f9b83e998a237ce42e1da7e35fe
mcakyerima <mcakyerima@gmail.com>
2023-05-05 01:44:13 +0100

    refactored_2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -15,22 +15,16 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
 	unsigned long int mask = 1;
-	while ((n >> 1) >= mask)
-		mask <<= 1;
+	int i, len = sizeof(n) * 8;
 
-	while (mask != 0)
+	for (i = len - 1; i >= 0; i--)
 	{
-		if (n & mask)
+		if (n & (mask << i))
 			_putchar('1');
-		else
+		else if (i == 0)
 			_putchar('0');
-		mask >>= 1;
+		else
+			continue;
 	}
 }
