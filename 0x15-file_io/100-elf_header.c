1e8f508cc57f5f6d92b17a57d4db7ecef6f69c0c
mcakyerima <mcakyerima@gmail.com>
2023-05-14 01:02:11 +0100

    output-fix

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -270,7 +270,7 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: %s <ELF file>\n", argv[0]);
-		return (EXIT_FAILURE);
+		exit(98);
 	}
 
 	if (readelf(argv[1], &ehdr) != 0)
