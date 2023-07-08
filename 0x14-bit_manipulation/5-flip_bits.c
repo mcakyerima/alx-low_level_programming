234aab03443bd3f11bbec5fff52400fddee9f185
mcakyerima <mcakyerima@gmail.com>
2023-07-08 01:07:10 +0100

    flip_bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,31 @@
+/*
+ * File_name: 5-flip_bits.c
+ * Author: Mohammed Ak Yerima.
+ * Description: Returns the number of bits needed to flip to get from
+ *		one number to another.
+ */
+
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits to flip to get from one
+ *		number to another
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int flip = n ^ m;
+	unsigned int count = 0;
+
+	while (flip)
+	{
+		count += flip & 1;
+		flip >>= 1;
+	}
+
+	return (count);
+}
